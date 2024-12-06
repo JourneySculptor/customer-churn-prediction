d08c4a0790dce00eb808ce8a27a3298e6b1463a3
JourneySculptor <180022223+JourneySculptor@users.noreply.github.com>
2024-12-06 23:28:08 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import uvicorn
+from uvicorn import run
 import os
 from api import app  # Import the FastAPI app instance from api.py
 
 if __name__ == "__main__":
     # Run the FastAPI app using Uvicorn as the ASGI server
-    port = int(os.environ.get("PORT", 8000))  # Run on 0.0.0.0 to make it accessible externally
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8000))   # Run on 0.0.0.0 to make it accessible externally
+    run("main:app", host="0.0.0.0", port=port)
